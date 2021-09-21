67fe48f8a33c6e0bf205957ac26d3d80d1dac22a
王教鼎 <onejiaoding@gmail.com>
2021-09-21 23:45:11 +0800

    add program list 5.1

---
+++ chapter_5/program_lists/forloop.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/9/21 11:41 下午
+ * @Description: 程序清单5.1
+ */
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int i;
+    for (i = 0; i < 5; i++)
+        cout << "C++ knows loops.\n";
+    cout << "C++ knows when to stop.\n";
+    return 0;
+}
