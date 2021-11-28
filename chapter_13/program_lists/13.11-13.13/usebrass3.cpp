e3bd7902dc80d8ebe420b61b5441c83c6bd54cec
王教鼎 <onejiaoding@gmail.com>
2021-11-28 20:52:04 +0800

    add program lists

---
+++ chapter_13/program_lists/13.11-13.13/usebrass3.cpp
@@ -0,0 +1,63 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/11/28 8:51 下午
+ * @Description: 程序清单13.13
+ */
+#include <iostream>
+#include <string>
+#include "acctabc.h"
+
+const int CLIENTS = 4;
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    AcctABC * p_clients[CLIENTS];
+    std::string temp;
+    long tempnum;
+    double tempbal;
+    char kind;
+
+    for (int i = 0; i < CLIENTS; i++)
+    {
+        cout << "Enter client's name: ";
+        std::getline(cin, temp);
+        cout << "Enter client's account number: ";
+        cin >> tempnum;
+        cout << "Enter opening balance: $";
+        cin >> tempbal;
+        cout << "Enter 1 for Brass Account or 2 for BrassPlus Account: ";
+        while (cin >> kind && (kind != '1' && kind != '2'))
+            cout << "Enter either 1 or 2: ";
+        if (kind == '1')
+            p_clients[i] = new Brass(temp, tempnum, tempbal);
+        else
+        {
+            double tmax, trate;
+            cout << "Enter the overdraft limit: $";
+            cin >> tmax;
+            cout << "Enter the interest rate as a decimal fraction: ";
+            cin >> trate;
+            p_clients[i] = new BrassPlus(temp, tempnum, tempbal, tmax, trate);
+        }
+        while (cin.get() != '\n')
+            continue;
+    }
+    cout << endl;
+    for (int i = 0; i < CLIENTS; i++)
+    {
+        p_clients[i]->ViewAcct();
+        cout << endl;
+    }
+
+    for (int i = 0; i < CLIENTS; i++)
+    {
+        delete p_clients[i];
+    }
+    cout << "Done.\n";
+
+    return 0;
+}
