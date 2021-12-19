3249eb07e9b52e756057df3484f6154f8f159598
王教鼎 <onejiaoding@gmail.com>
2021-12-19 23:42:17 +0800

    add program list

---
+++ chapter_17/program_lists/cinexcp.cpp
@@ -0,0 +1,30 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2021/12/19 11:33 下午
+ * @Description: 程序清单17.12
+ */
+#include <iostream>
+#include <exception>
+
+int main()
+{
+    using namespace std;
+    cin.exceptions(ios_base::failbit);
+    cout << "Enter numbers: ";
+    int sum = 0;
+    int input;
+    try {
+        while (cin >> input)
+        {
+            sum += input;
+        }
+    } catch (ios_base::failure & bf)
+    {
+        cout << bf.what() << endl;
+        cout << "O! the horror!\n";
+    }
+
+    cout << "Last value entered = " << input << endl;
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
