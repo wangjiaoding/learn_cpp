9e35d2896a499b23e7fcde5d311b25ac18d1d10c
王教鼎 <onejiaoding@gmail.com>
2022-01-21 01:17:11 +0800

    add program list

---
+++ chapter_15/program_lists/15.1-15.3/tv.cpp
@@ -0,0 +1,59 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2022-01-19 23:39:56
+ * @Description: 程序清单15.2
+ */
+#include <iostream>
+#include "tv.h"
+
+bool Tv::volup()
+{
+    if (volume < MaxVal)
+    {
+        volume++;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool Tv::voldown()
+{
+    if (volume > MinVal)
+    {
+        volume--;
+        return true;
+    }
+    else
+        return false;
+}
+
+void Tv::chanup()
+{
+    if (channel < maxchannel)
+        channel++;
+    else
+        channel = 1;
+}
+
+void Tv::chandown()
+{
+    if (channel > 1)
+        channel--;
+    else
+        channel = maxchannel;
+}
+
+void Tv::settings() const
+{
+    using std::cout;
+    using std::endl;
+    cout << "TV is " << (state == Off ? "Off" : "On") << endl;
+    if (state == On)
+    {
+        cout << "Volume setting = " << volume << endl;
+        cout << "Channel setting = " << channel << endl;
+        cout << "Mode = " << (mode == Antenna ? "antenna" : "cable") << endl;
+        cout << "Input = " << (input == TV ? "TV" : "DVD") << endl;
+    }
+}
---
+++ chapter_15/program_lists/15.1-15.3/use_tv.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Author: 王教鼎
+ * @Date: 2022-01-20 00:11:47
+ * @Description: 程序清单15.3
+ */
+#include <iostream>
+#include "tv.h"
+
+int main()
+{
+    using namespace std;
+    Tv s42;
+    cout << "Initial settings for 42\" TV:\n";
+    s42.settings();
+    s42.onoff();
+    s42.chanup();
+    cout << "\nAdjusted settings for 42\" TV:\n";
+    s42.settings();
+
+    Remote grey;
+    grey.set_chan(s42, 10);
+    grey.volup(s42);
+    grey.volup(s42);
+    cout << "\n42\" settings after using remote:\n";
+    s42.settings();
+
+    Tv s58(Tv::On);
+    s58.set_mode();
+    grey.set_chan(s58, 28);
+    cout << "\n58\" settings:\n";
+    s58.settings();
+    return 0;
+}
